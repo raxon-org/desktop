befd2c2bd6162fa64e81fa50e94f8d8ac67091e5
Remco van der Velde <remco@universeorange.com>
2026-05-30 20:29:44 +0200

    init

--- src/Application/Public/Module/Navigation.js
+++ src/Application/Public/Module/Navigation.js
@@ -10,6 +10,7 @@ navigation.init = (id) => {
     let active_user = user.get();
     console.log(active_user);
     console.log(document.cookie);
+    alert('cookie');
     let url;
     if(is.empty(active_user)){
         url = file.data.get('route.backend.user.current');
