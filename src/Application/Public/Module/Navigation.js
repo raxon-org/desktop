0da291cc3bf1fc61b72dcff4c7368adb4958fde5
Remco van der Velde <remco@universeorange.com>
2026-08-22 18:16:42 +0200

    init

--- src/Application/Public/Module/Navigation.js
+++ src/Application/Public/Module/Navigation.js
@@ -29,10 +29,13 @@ navigation.init = (id) => {
                             });
                         });
                     } else {
-                        !is.empty(response.class) &&
-                        in_array(response.class, [
-                            'Raxon\\Exception\\AuthorizationException',
-                        ]){
+                        if(
+                            !is.empty(response.class) &&
+                            in_array(
+                            response.class, [
+                                'Raxon\\Exception\\AuthorizationException',
+                            ])
+                        ){
                             url = file.data.get('route.frontend.user.login');
                             redirect(url);
                         }
