009a64ee9bad138388171edd6e4575b29a610499
Remco van der Velde <remco@universeorange.com>
2026-08-25 19:25:36 +0200

    init

--- src/Application/Public/Module/Navigation.js
+++ src/Application/Public/Module/Navigation.js
@@ -19,6 +19,7 @@ navigation.init = (id) => {
                         response?.class === 'Raxon\\Exception\\AuthorizationException' &&
                         response?.message === 'Expired token...'
                     ) {
+                        priya.debug(response?.message);
                         url = file.data.get('route.frontend.user.login');
                         console.warn('load authentication mechanism');
                         redirect(url);
