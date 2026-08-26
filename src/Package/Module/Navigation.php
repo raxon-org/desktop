884a0a9dba9be7c84b411343ef000054fec5040e
Remco van der Velde <remco@universeorange.com>
2026-08-27 00:20:01 +0200

    init

--- src/Package/Module/Navigation.php
+++ src/Package/Module/Navigation.php
@@ -12,17 +12,17 @@ class Navigation
      * @throws ObjectException
      * @throws Exception
      */
-    public static function create(App $object, array $list=[], object $options=null): void
+    public static function create(App $object, array $list=[], object|null $options=null): void
     {
         if(!property_exists($options, 'name')){
-            return;
+            throw new Exception('$options name is required.');
         }
         if(property_exists($options, 'route') && property_exists($options->route, 'name')){
             //nothing
         } else {
-            return;
-        }
+            throw new Exception('$options route->name is required.');
 
+        }
         foreach($list as $nr => $user){
             if(
                 is_object($user) &&
